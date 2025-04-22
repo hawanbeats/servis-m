fb93267cc9a319b7e6a842dc3cfdc1f0f2f1ff73
Hasan Damirli <72316307+hawanbeats@users.noreply.github.com>
2025-04-22 21:59:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,6 +45,59 @@
         return false;
     });
 
+    $('#sponsor-carousel').owlCarousel({
+        loop: true,
+        margin: 5,
+        center: false,
+        autoplay: true,
+        smartSpeed: 500,
+        nav: false,
+        navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>'],
+        dots: false,
+        responsive : {
+            0 : {
+                items: 2
+            },
+            480 : {
+                items: 3,
+            },
+            768 : {
+                items: 3,
+            },
+            992 : {
+                items: 6,
+            }
+        }
+    });
+
+    $(document).ready(function() {
+        // When a product is clicked, show the modal with detailed info
+        $('.product-item').on('click', function() {
+            const productId = $(this).data('product');
+            
+            // Use productId to fetch product details dynamically (hardcoded for now)
+            const productDetails = {
+                1: {
+                    name: 'Product 1',
+                    image: 'path_to_image1.jpg',
+                    description: 'Detailed description of Product 1.',
+                    price: '$29.00'
+                },
+                // Add other products here as needed
+            };
+    
+            const details = productDetails[productId];
+    
+            // Update modal content
+            $('#product-name').text(details.name);
+            $('#product-detail-image').attr('src', details.image);
+            $('#product-description').text(details.description);
+            $('#product-price').text(details.price);
+    
+            // Show the modal
+            $('#product-detail-modal').modal('show');
+        });
+    });
+    
 
 })(jQuery);
-
